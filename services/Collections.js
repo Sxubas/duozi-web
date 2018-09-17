4407d7d8b3f39172380ab360ee85e682b4707b71
Sergio Cárdenas <se.cardenas@uniandes.edu.co>
2018-09-16 23:37:07 -0500

    back finished.

--- services/Collections.js
+++ services/Collections.js
@@ -24,7 +24,9 @@ Collections.addWord = (req, res, db) => {
         'dateCreated': Date.now(),
         'simplified': req.body.simplified,
         'traditional': req.body.traditional,
-        'pinyins': req.body.pinyins
+        'pinyins': req.body.pinyins,
+        'categories': req.body.categories,
+        'def': req.body.def
       }}}, {returnOriginal: false}, (err, r) => {
         if(err) {
           res.status(500).send(err);
@@ -34,12 +36,14 @@ Collections.addWord = (req, res, db) => {
     }
     else {
       db.collection('collections').insertOne({'email': req.body.email, characters: [{
-        'id': new ObjectID(),
+        '_id': new ObjectID(),
         'dateCreated': Date.now(),
         'simplified': req.body.simplified,
         'traditional': req.body.traditional,
-        'pinyins': req.body.pinyins
-      }], 'categories': []}, (err, r) => {
+        'pinyins': req.body.pinyins,
+        'categories': req.body.categories,
+        'def': req.body.def
+      }]}, (err, r) => {
         if(err) res.send(err);
         else res.send(r.ops[0]);
       });
@@ -55,7 +59,13 @@ Collections.modifyWord = (req, res, db) => {
       res.send('User or character not found.');
     }
     else {
-      db.collection('collections').findOneAndUpdate({'email': req.body.email, 'characters._id': new ObjectID(req.body._id)}, {$set: {'characters.$.simplified': req.body.simplified===undefined?r.characters.find(x => x.id === req.body.id).simplified:req.body.simplified, 'characters.$.traditional': req.body.traditional===undefined?r.characters.find(x => x.id === req.body.id).traditional:req.body.traditional, 'characters.$.pinyins': req.body.pinyins===undefined?r.characters.find(x => x.id === req.body.id).pinyins:req.body.pinyins}}, {returnOriginal: false}, (err2, r2) => {
+      db.collection('collections').findOneAndUpdate({'email': req.body.email, 'characters._id': new ObjectID(req.body._id)}, {$set: {
+        'characters.$.simplified': req.body.simplified===undefined?r.characters.find(x => x._id === req.body._id).simplified:req.body.simplified, 
+        'characters.$.traditional': req.body.traditional===undefined?r.characters.find(x => x._id === req.body._id).traditional:req.body.traditional, 
+        'characters.$.pinyins': req.body.pinyins===undefined?r.characters.find(x => x._id === req.body._id).pinyins:req.body.pinyins,
+        'characters.$.categories': req.body.categories===undefined?r.characters.find(x => x._id === req.body._id).categories:req.body.categories,
+        'characters.$.def': req.body.def===undefined?r.characters.find(x => x._id === req.body._id).def:req.body.def
+      }}, {returnOriginal: false}, (err2, r2) => {
         if(err2) res.status(500).send(err2);
         else res.send(r2.value);
       });
